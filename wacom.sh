119102f0d056bb315ae41da185c50887b7c5d851
kammradt <vinicius.kammradt1@gmail.com>
2021-10-24 13:13:48 -0300

    adds wacom.sh script

---
+++ wacom.sh
@@ -0,0 +1,4 @@
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 1 "key ctrl z"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 2 "key ctrl d"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 3 "key ctrl c"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 8 "key ctrl v"
